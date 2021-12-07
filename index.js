1948c5a8fa391e9c6ae29dc137e074db1d2d4d1c
Iryna Kutsenko <79517296+irynakutsenko@users.noreply.github.com>
2021-12-07 22:05:34 +0100

    new one

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.use(express.json({extenderd: true}))
-
 app.use('/api/auth', require('./routes/auth.route'))
 app.use('/api/todo', require('./routes/todo.route'))
 
